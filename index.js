dcbe13d2a170fd869c3fe9eedf9884d69b0e29c3
patatrak <zepangolin@gmail.com>
2017-06-05 19:45:10 +0200

    change a few thing just to test a few thing :D

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 var express = require('express');
-var app = express();
+var srv = express();
 
-app.get('/hello', function (req, res) {
-  res.send('Hello !');
-});
-
-app.get('/bonjour', function (req, res) {
-  res.send('Bonjour !');
-});
-
-app.get('/', function (req, res) {
+srv.get('/', function (req, res) {
   res.send("Salut !");
 });
 
 
-app.get('/test', function (req, res) {
+srv.get('/test', function (req, res) {
 	res.send("<h1>Est-ce que tu sais vraiment ce que tu fais ?</h1>");
 });
 
 var port = process.env.PORT || 3000;
 
-app.listen(port, function () {
+srv.listen(port, function () {
   console.log('Server running on port '+port);
 });
