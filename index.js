a746c37f434169d8fe35607939d2b358b326e9a1
patatrak <zepangolin@gmail.com>
2017-06-05 19:55:07 +0200

    Fix a typo in the message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ srv.get('/', function (req, res) {
 
 
 srv.get('/test', function (req, res) {
-	res.send("<h1>This should have been delpolyed automaticaly after I git push</h1>");
+	res.send("<h1>This should have been deployed automaticaly after I git push</h1>");
 });
 
 var port = process.env.PORT || 3000;
