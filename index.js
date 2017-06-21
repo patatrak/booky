eddc6bfc3740e9b0fb6953b71b17ac2908205f0b
patatrak <zepangolin@gmail.com>
2017-06-21 21:50:06 +0200

    First small exo from openclassroom : add a module

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var srv = express();
 
+var test = require('oc_test');
+
 srv.get('/', function (req, res) {
   res.send("Salut !");
 });
 
 
 srv.get('/test', function (req, res) {
-	res.send("<h1>This should have been deployed automaticaly after I git push</h1>");
+	res.send("<h1>This should have been deployed automaticaly after I git push</h1></br>" + test.hello());
 });
 
 var port = process.env.PORT || 3000;
