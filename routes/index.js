1642caa3be68af2e644f83b421d343aad9e91ff2
patatrak <zepangolin@gmail.com>
2017-09-03 18:32:22 +0200

    Modify some route and template as an exercise from MDN

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Booky',
-								description: 'My first attempt for a social bookshelf, based on Node js and Express',
-								heroku: 'heroku.com'});
+								description: 'my first attempt for a social bookshelf, based on Node js and Express',
+								host: 'heroku.com'});
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', {	title: 'First modification of the skeleton',
+								message: 'Mozilla dev network is so cool'} );
 });
 
 module.exports = router;
