96de4d47d160f3c5eb8083fe679c869ef296a363
patatrak <zepangolin@gmail.com>
2017-11-02 20:29:15 +0100

    Look for a book from its ISBN. Does not work well with french books

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var isbn = require('node-isbn');
+
 var debug = require('debug')('booky:books');
 
 /*Look for a book by ISBN : form */
@@ -9,9 +11,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/lookup', function(req, res, next) {
-  debug('\r\n\tisbn is :' + req.body.isbn);
-  res.render('books', { title: `Information on book: ${req.body.isbn}`, 
-								isbn: req.body.isbn} );
+  debug('ISBN to look for:' + req.body.isbn);
+  
+	isbn.resolve(req.body.isbn, function (err, book) {
+		if (err) {
+			debug('Book not found', err);
+			res.render('books', {
+										title: `Book: {req.body.isbn}`, 
+										isbn : req.body.isbn} );
+		} else {
+			debug(`Found: ${book.title}`);
+			res.render('books', {
+										title		: `${book.title} by ${book.authors}`, 
+										isbn		: req.body.isbn,
+										bookTitle: book.title,
+										pic		: book.imageLinks.thumbnail} );
+		}
+	});
+  
+  
 });
 
 module.exports = router;
