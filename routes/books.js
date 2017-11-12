ac002277e8659c827c3d25edbff34767c4e42488
patatrak <zepangolin@gmail.com>
2017-11-12 13:54:51 +0100

    isbn lookup: better use of the json when displaying result. Add a debug function to display a json (will need to be removed or moved)

--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,32 @@ var isbn = require('node-isbn');
 
 var debug = require('debug')('booky:books');
 
+function objectInspector(object, result) {
+    if (typeof object != "object")
+        return "Invalid object";
+    if (typeof result == "undefined")
+        result = '';
+/*
+    if (result.length > 50)
+        return "[RECURSION TOO DEEP. ABORTING.]";*/
+
+    var rows = [];
+    for (var property in object) {
+        var datatype = typeof object[property];
+
+        var tempDescription = result+'"'+property+'"';
+        tempDescription += ' ('+datatype+') => ';
+        if (datatype == "object")
+            tempDescription += 'object : '+objectInspector(object[property],result+'  ');
+        else
+            tempDescription += object[property];
+
+        rows.push(tempDescription);
+    }
+
+    return rows.join(result+"\n");
+}
+
 /*Look for a book by ISBN : form */
 router.get('/', function(req, res, next) {
   res.render('books', {	title: 'Lookging for a book'} );
@@ -21,12 +47,13 @@ router.get('/lookup', function(req, res, next) {
 										isbn : req.query.isbn} );
 		} else {
 			debug(`Found: ${book.title}`);
-			/*debug("Full json %j", book);*/
+			/*debug("Full json %o", book);*/
+			debug(objectInspector(book));			
+			
 			res.render('books', {
 										title		: book.title, 
 										isbn		: req.query.isbn,
-										bookTitle: `${book.title} by ${book.authors}`,
-										pic		: (typeof book.imageLinks != "undefined" ? book.imageLinks.thumbnail : undefined)} );
+										obj		: book } );
 		}
 	});
   
